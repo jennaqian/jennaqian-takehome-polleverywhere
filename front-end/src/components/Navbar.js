0f4b72c6523ca1f3d196ccba20d798ab865e5471
Jenna Qian <jiayin1996@gmail.com>
2023-01-18 12:54:16 -0500

    added css for navbar

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom"
 export default function Navbar ({id}){
 
     return(
-        <nav>
-                <Link to={`/`}>All Raffles</Link>
-                <Link to={`/raffles/${id}`}>Register</Link>
-                <Link to={`/raffles/${id}/participants`}>Participants</Link>
-                <Link to={`/raffles/${id}/winner`}>Pick Winner</Link>
+        <nav className="navbar">
+                <Link className="navbar-links" to={`/`}>All Raffles</Link>
+                <Link className="navbar-links" to={`/raffles/${id}`}>Register</Link>
+                <Link className="navbar-links" to={`/raffles/${id}/participants`}>Participants</Link>
+                <Link className="navbar-links" to={`/raffles/${id}/winner`}>Pick Winner</Link>
         </nav>
     )
     
